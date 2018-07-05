b15329b623e7999289dbc734fe92b710d7a7bf59
Diogo Henrique Camilo dos Santos <38320313+diogocamilosquadra@users.noreply.github.com>
2018-07-05 18:11:09 -0300

    Update Cliente.cs

--- Loja de produtos/Loja de produtos/Cliente.cs
+++ Loja de produtos/Loja de produtos/Cliente.cs
@@ -6,7 +6,7 @@ namespace Loja_de_produtos
 {
     class Cliente
     {
-
+        public int codigoCliente;
         public string nomeCliente;
         public string endereco;
         public string telefone;
