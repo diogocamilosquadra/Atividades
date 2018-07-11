d866584d6e91d6408007685b204ef0f2a447b525
Diogo Henrique Camilo dos Santos <38320313+diogocamilosquadra@users.noreply.github.com>
2018-07-11 18:13:42 -0300

    Add files via upload

---
+++ Teste NHibernate/Teste NHibernate/NHibernateTutorialPart1/NHibernateTutorialPart1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace NHibernateTutorialPart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(98, 71);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstName.TabIndex = 0;
+            this.txtFirstName.TextChanged += new System.EventHandler(this.txtFirstName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Primeiro Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ultimo nome";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(98, 106);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(100, 20);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(61, 145);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(163, 23);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(61, 174);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(163, 23);
+            this.btnDeletar.TabIndex = 5;
+            this.btnDeletar.Text = "Deletar";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Listar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 232);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Atualizar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(98, 45);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(31, 20);
+            this.txtID.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 289);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Teste Hibernate";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtID;
+    }
+}
+
---
+++ Teste NHibernate/Teste NHibernate/NHibernateTutorialPart1/NHibernateTutorialPart1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NHibernateTutorialPart1;
+
+namespace NHibernateTutorialPart1
+{
+    public partial class Form1 : Form
+    {
+        private Configuration myConfiguration;
+        private ISessionFactory mySessionFactory;
+        private ISession mySession;
+   
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //Declara as variaveis do Nhibernate
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+
+            //  Salva os itens
+            using (mySession.BeginTransaction())
+            {
+                Contact loContact = new Contact();
+                loContact.ID = int.Parse(txtID.Text);
+                loContact.FirstName = txtFirstName.Text;
+                loContact.LastName = txtLastName.Text;
+                mySession.Save(loContact);
+
+                mySession.Transaction.Commit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Declara as variaveis do Nhibernate
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+
+            //Deleta de acordo com a sintaxe
+            using (mySession.BeginTransaction())
+            {
+                Contact loContact = new Contact();
+                loContact.FirstName = txtFirstName.Text;
+                loContact.LastName = txtLastName.Text;
+                mySession.Delete("from Contact where FirstName='"+loContact.FirstName+"'" +
+                "and LastName='"+loContact.LastName+"';");
+
+                mySession.Transaction.Commit();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            //Declara as variaveis do Nhibernate
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+
+            //Lista todos os itens da classe Contact
+            using (mySession.BeginTransaction())
+            {
+
+                ICriteria criteria = mySession.CreateCriteria<Contact>();
+                IList<Contact> list = criteria.List<Contact>();
+                
+                foreach (Contact pessoa in list)
+                { 
+                    MessageBox.Show(" ID da pessoa: " + pessoa.ID +
+                                    " \nPrimeiro nome da pessoa: " + pessoa.FirstName +
+                                    " \nSegundo  nome da pessoa: " + pessoa.LastName
+                                    );
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Declara as variaveis do Nhibernate
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+
+            //Atualiza um cadastro
+            using (mySession.BeginTransaction())
+            {
+
+                Contact loContact = new Contact();
+
+                loContact.ID = int.Parse(txtID.Text);
+                loContact.FirstName = txtFirstName.Text;
+                loContact.LastName = txtLastName.Text;
+
+                mySession.SaveOrUpdate(loContact);
+                mySession.Transaction.Commit();
+            }
+
+        }
+
+        private void txtFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
